0cbcbe7c10d1859a82b483c79de0fc705f55daa1
Crosscode <crosscode@ip-172-31-45-151.us-west-2.compute.internal>
2019-01-25 07:02:59 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
-          </p>
+             Hello Crosscode !
+        </p>
           <a
             className="App-link"
             href="https://reactjs.org"
